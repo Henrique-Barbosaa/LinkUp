3b0159b66a07316467ddb843e8a1d5aac91742e5
Antonio Henrique <henrique1508cops@gmail.com>
2026-01-11 14:56:54 -0300

    Conteinerização completa da aplicação

--- src/main/java/com/linkup/config/HashConfig.java
+++ src/main/java/com/linkup/config/HashConfig.java
@@ -1,6 +1,7 @@
 package com.linkup.config;
 
 import org.hashids.Hashids;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class HashConfig {
 
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final String SALT = "3d62b621-56cb-45cd-b38f-42843888b1a9";
+
+    @Value("${app.hashids.salt}")
+    private String salt;
 
     @Bean
     public Hashids hashids() {
-        return new Hashids(SALT, 4, CHARACTERS);
+        return new Hashids(salt, 4, CHARACTERS);
     }
 }
